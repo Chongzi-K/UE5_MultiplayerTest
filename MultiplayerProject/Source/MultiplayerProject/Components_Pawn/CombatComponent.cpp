0bb8577542d0310a004b0858f26e381f78fe93a3
Chongzi-K <xie-zi-wei@foxmail.com>
2022-10-04 20:12:09 +0800

    20221004-4 添加Aiming和Shooting时的准星变化

--- MultiplayerProject/Source/MultiplayerProject/Components_Pawn/CombatComponent.cpp
+++ MultiplayerProject/Source/MultiplayerProject/Components_Pawn/CombatComponent.cpp
@@ -134,6 +134,10 @@ void UCombatComponent::FireButtonPressed(bool bPressed)
 		FHitResult HitResult;
 		TraceUnderCrosshairs(HitResult);
 		ServerFire(HitResult.ImpactPoint);
+		if (EquippedWeapon)
+		{
+			CrosshairShootingFactor = 0.8f;
+		}
 	}
 
 }
@@ -211,8 +215,23 @@ void UCombatComponent::SetHUDCrosshairs(float DeltaTime)
 			{
 				CrosshairInAirFactor = FMath::FInterpTo(CrosshairInAirFactor, 0.0f, DeltaTime, 30.0f);
 			}
+			if (bAiming)
+			{
+				CrosshairAimFactor = FMath::FInterpTo(CrosshairAimFactor, 0.6f, DeltaTime, 30.0f);
+			}
+			else
+			{
+				CrosshairAimFactor = FMath::FInterpTo(CrosshairAimFactor, 0.0f, DeltaTime, 30.0f);
+			}
+
+			CrosshairShootingFactor = FMath::FInterpTo(CrosshairShootingFactor, 0.0f, DeltaTime, 40.0f);
 			
-			HUDPackage.CrosshairSpread = CrosshairVelocityFactor + CrosshairInAirFactor;
+			HUDPackage.CrosshairSpread = 
+				0.5f +
+				CrosshairVelocityFactor +
+				CrosshairInAirFactor - 
+				CrosshairAimFactor + 
+				CrosshairShootingFactor;
 			HUD->SetHUDPackage(HUDPackage);
 		}
 	}
